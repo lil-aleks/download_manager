2490e1fd446c0b7a6aa914a689eb29dab42b7be7
lil-aleks <olekiwan4@gmail.com>
2026-03-31 13:53:09 +0200

    Feat: Add flex to window

--- src/components/AppWindow.tsx
+++ src/components/AppWindow.tsx
@@ -4,7 +4,7 @@ const AppWindow: Component<{ children?: JSXElement }> = (
   props
 ) => {
   return (
-    <div class="w-dvw h-dvh overflow-hidden p-2 bg-linear-to-br from-blue-400/40 to-35% to-transparent">
+    <div class="w-dvw h-dvh overflow-hidden flex flex-col justify-between p-2 bg-linear-to-br from-blue-400/40 to-35% to-transparent">
       {props.children}
     </div>
   );
