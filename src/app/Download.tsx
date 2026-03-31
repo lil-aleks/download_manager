2dd5c3d7e246eb39043275104af53b6d24e85477
lil-aleks <olekiwan4@gmail.com>
2026-03-31 11:21:30 +0200

    Feat: Add AppWindow and Header Components in Apps

--- src/app/Download.tsx
+++ src/app/Download.tsx
@@ -1,8 +1,14 @@
 import { Component } from "solid-js";
+import AppWindow from "../components/AppWindow";
+import Header from "../components/Header";
 
 const App: Component = () => {
   return (
-    <></>
+    <AppWindow>
+      <Header title="New Download">
+        {/* TODO: Buttons in header here */}
+      </Header>
+    </AppWindow>
   )
 }
 
--- src/app/Settings.tsx
+++ src/app/Settings.tsx
@@ -1,8 +1,14 @@
 import { Component } from "solid-js";
+import Header from "../components/Header";
+import AppWindow from "../components/AppWindow";
 
 const App: Component = () => {
   return (
-    <></>
+    <AppWindow>
+      <Header title="Settings">
+        {/* TODO: Buttons in header here */}
+      </Header>
+    </AppWindow>
   )
 }
 
