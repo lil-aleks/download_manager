c308b293ff7566418336de0db968cb7fe730d942
lil-aleks <olekiwan4@gmail.com>
2026-03-31 11:16:17 +0200

    Feat: Add basic header ui

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Component, JSXElement } from "solid-js";
+
+const Header: Component<{ title: string, children?: JSXElement }> = (
+  props
+) => {
+  return (
+    <div class="w-full flex flex-row gap-2 p-2 justify-between">
+      <div data-tauri-drag-region class="flex flex-row items-center gap-1 w-full">
+        <img src="src/assets/logo.png" class="w-6"/>
+        <h1 class="text-xl font-bold">{props.title}</h1>
+      </div>
+      <div class="flex flex-row gap-2 content-center items-center">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
